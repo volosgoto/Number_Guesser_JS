4a38c94a1427c1d898df86f8f50395c0ef88fcaa
volosgoto <volosgoto@yahoo.com>
2018-08-01 11:37:32 +0300

    add game and UI vars

---
+++ app.js
@@ -0,0 +1,15 @@
+// Game vars
+let min = 1,
+    max = 10,
+    winningNum = 5,
+    guessesLeft = 3;
+
+// UI vars
+let game = document.querySelector('#game');
+let minNum = document.querySelector('.min-num');
+let minMax = document.querySelector('.max-num');
+let guessBtn = document.querySelector('#guess-btn');
+let guessInput = document.querySelector('#guess-input');
+let message = document.querySelector('.message');
+
+
