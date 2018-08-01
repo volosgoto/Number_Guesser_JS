a3939476ca90eee3a21938a030bf58a064aec93d
volosgoto <volosgoto@yahoo.com>
2018-08-01 12:17:32 +0300

    add error msg

--- app.js
+++ app.js
@@ -7,9 +7,32 @@ let min = 1,
 // UI vars
 let game = document.querySelector('#game');
 let minNum = document.querySelector('.min-num');
-let minMax = document.querySelector('.max-num');
+let maxNum = document.querySelector('.max-num');
 let guessBtn = document.querySelector('#guess-btn');
 let guessInput = document.querySelector('#guess-input');
 let message = document.querySelector('.message');
+
+// Assign UI min, max
+minNum.textContent = min;
+maxNum.textContent = max;
+
+// Listen for guess
+guessBtn.addEventListener('click', ()=>{
+    let guess = parseInt(guessInput.value);
+    console.log(guess);
+
+    // Validate
+    if ( isNaN(guess) || guess < min || guess > max){
+        setMessage(`Please enter your number between ${min} and ${max}`);
+    }
+})
+
+// Set message
+function setMessage(msg) {
+    message.style.color = 'red';
+    message.textContent = msg;
+}
+
+
 
 
